805847d65ec8e356ee159c6ef8d3184d3d722e6f
prathamnemade <prathameshnemade95@gmail.com>
2019-06-08 00:38:03 +0530

    trying to deploy7

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/Sekel-Tech'));
+app.use(express.static(__dirname + '/dist/SekelTech'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/Sekel-Tech/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/SekelTech/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
